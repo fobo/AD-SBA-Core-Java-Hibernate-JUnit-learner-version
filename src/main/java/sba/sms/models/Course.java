9cc15c887c76b2793a5c8fb7b7e18bf10a79810a
Kevin Diehr <kddiehr@gmail.com>
2023-01-26 21:08:53 -0500

    added boilerplate for course and student, getters setters and field

--- src/main/java/sba/sms/models/Course.java
+++ src/main/java/sba/sms/models/Course.java
@@ -1,5 +1,40 @@
 package sba.sms.models;
 
-public class Course {
+import java.util.List;
 
+public class Course {
+	//fields
+	int id;
+	String name;
+	String instructor;
+	List<Student> students;
+	//constructors
+	
+	//other methods
+	
+	//getters setters
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getInstructor() {
+		return instructor;
+	}
+	public void setInstructor(String instructor) {
+		this.instructor = instructor;
+	}
+	public List<Student> getStudents() {
+		return students;
+	}
+	public void setStudents(List<Student> students) {
+		this.students = students;
+	}
 }
--- src/main/java/sba/sms/models/Student.java
+++ src/main/java/sba/sms/models/Student.java
@@ -1,15 +1,44 @@
 package sba.sms.models;
 
+import java.util.List;
+
 public class Student {
 	//fields email, name, password, courses
 	String email;
+	String name;
+	String password;
+	List<Course> courses;
 	
 	//constructors
 	
 	//other methods
 	
-	//getters
+	//getters and setters
 	
-	//setters
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public List<Course> getCourses() {
+		return courses;
+	}
+	public void setCourses(List<Course> courses) {
+		this.courses = courses;
+	}
+	
 	
 }
