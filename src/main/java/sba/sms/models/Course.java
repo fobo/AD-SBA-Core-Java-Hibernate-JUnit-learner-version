9fa3dac4798a308847d5a95587824264a137a4a0
Kevin Diehr <kddiehr@gmail.com>
2023-01-28 02:30:04 -0500

    Cleaned and finished models. Added table and columns names and manytomany connections. Improved commenting for references to useful websites for future capstone use.

--- src/main/java/sba/sms/models/Course.java
+++ src/main/java/sba/sms/models/Course.java
@@ -1,6 +1,9 @@
 package sba.sms.models;
 
 import java.util.List;
+import java.util.Objects;
+
+import jakarta.persistence.*;
 
 import lombok.*;
 
@@ -10,18 +13,36 @@ import lombok.*;
 @ToString
 
 /*TO DO:
- * Add HQL
- * Finish lombok annotations (declare table, and entity
- * declare column names and lengths for the fields
- * create many to many relationship between the two models (figure that out later, check slides and example project)
- * Add equals method
- * Add hashcode method
+ * Add HQL --MOVED TO SERVICES
+ * Finish lombok annotations -- DONE
+ * Add HQL annotations (declare table, and entity) -- DONE
+ * declare column names and lengths for the fields -- DONE
+ * create many to many relationship between the two models (figure that out later, check slides and example project) -- DONE
+ * https://www.baeldung.com/jpa-cascade-types
+ * 
+ * Add equals method -- DONE
+ * Add hashcode method -- DONE
  */
+@Table(name = "course")
+@Entity
 public class Course {
 	//fields
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)//automatically generates the primary key value, defaults to AUTO
+	//@NonNull
+	@Column(name = "id")
 	private int id;
+	
+	@Column(length = 50, name = "name")
+	@NonNull
 	private String name;
+	
+	@Column(length = 50, name = "instructor")
 	private String instructor;
+	
+	// fetch type eager, cascade all except remove, mappedBy courses
+	@ToString.Exclude
+	@ManyToMany(mappedBy = "courses", cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH}, fetch = FetchType.EAGER)
 	private List<Student> students;
 	//constructors
 	public Course() {
@@ -42,7 +63,26 @@ public class Course {
 	}
 
 
-	//other methods
+	//other methods, auto generated from source
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, instructor, name);
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Course other = (Course) obj;
+		return id == other.id && Objects.equals(instructor, other.instructor) && Objects.equals(name, other.name);
+	}
+
+
 	
 	//getters setters
 }
--- src/main/java/sba/sms/models/Student.java
+++ src/main/java/sba/sms/models/Student.java
@@ -1,34 +1,56 @@
 package sba.sms.models;
 
 import java.util.List;
+import java.util.Objects;
 
+import jakarta.persistence.*;
 import lombok.*;
-
-
-
 
 @Setter
 @Getter
 @ToString
-/*TO DO:
- * Add HQL
- * Finish lombok annotations (declare table, and entity
- * declare column names and lengths for the fields
- * create many to many relationship between the two models (figure that out later, check slides and example project)
- * Add equals method
- * Add hashcode method
+/*
+ * TO DO: Add HQL 
+ * Finish lombok annotations
+ *  Add HQL annotations (declare table,
+ * and entity) 
+ * declare column names and lengths for the fields create many to
+ * many relationship between the two models (figure that out later, check slides
+ * and example project) Add equals method Add hashcode method
+ * ALL FINISHED
  */
+
+@Table(name = "student")
+@Entity
 public class Student {
-	//fields email, name, password, courses
+	// fields email, name, password, courses
+	@Id
+	@NonNull
+	@Column(length = 50, name = "email")
 	private String email;
+
+	@NonNull
+	@Column(length = 50, name = "name")
 	private String name;
+
+	@NonNull
+	@Column(length = 50, name = "password")
 	private String password;
+
+	/*
+	 * Join table strategy name student_courses , name of student primary key column
+	 * student_email and inverse primary key (courses) column courses_id , fetch
+	 * type eager, cascade all except remove
+	 */
+	@ToString.Exclude
+	@ManyToMany(cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH}, fetch = FetchType.EAGER)
+	@JoinTable(name = "student_courses", joinColumns = @JoinColumn(name = "student_email"), inverseJoinColumns = @JoinColumn(name = "courses_id"))
 	private List<Course> courses;
-	
-	//constructors
+
+	// constructors
 	public Student() {
 	}
-	
+
 	public Student(String email, String name, String password, List<Course> courses) {
 		this.email = email;
 		this.name = name;
@@ -42,9 +64,27 @@ public class Student {
 		this.password = password;
 	}
 
-	//other methods
 	
-	//getters and setters
+	
+	// other methods
+	@Override
+	public int hashCode() {
+		return Objects.hash(email, name, password);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Student other = (Student) obj;
+		return Objects.equals(email, other.email) && Objects.equals(name, other.name)
+				&& Objects.equals(password, other.password);
+	}
+
+
+	// getters and setters
 }
-	
-	
