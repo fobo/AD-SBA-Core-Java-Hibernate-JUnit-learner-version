bc2523c8a82ee177e96897beaaa0e5327f0fa892
Kevin Diehr <kddiehr@gmail.com>
2023-01-29 06:13:04 -0500

    Added comments for workflow in StudentService.Java

--- src/main/java/sba/sms/services/StudentService.java
+++ src/main/java/sba/sms/services/StudentService.java
@@ -17,36 +17,103 @@ import sba.sms.models.Student;
  */
 
 public class StudentService implements StudentI {
+	private static final CourseService courseService = new CourseService();
 
 
-
+	@Override
 	public void createStudent(Student student) {
 		// TODO Auto-generated method stub
+		/*
+		 * create new transaction object
+		 * create new session object
+		 * begin transaction
+		 * create query to insert data into DB
+		 * commit transaction
+		 * catch errors
+		 * no return data
+		 */
 		
 	}
 
-	public boolean validateStudent(String email, String password) {
+	public boolean validateStudent(String email, String password) { //checks for correct student-password relationship
 		// TODO Auto-generated method stub
+		/*
+		 * create new Student object
+		 * create new transaction object
+		 * create new session object
+		 * begin transaction
+		 * create query
+		 * fill Student object with data
+		 * check if data from object matches data input from user
+		 * commit transaction
+		 * return boolean vaule based on result
+		 * 
+		 */
+		
 		return false;
 	}
 
 	public Student getStudentByEmail(String email) {
 		// TODO Auto-generated method stub
+		
+		/*
+		 * create new Student object
+		 * create new transaction object
+		 * create new session object
+		 * begin transaction
+		 * create query
+		 * fill Student object with data
+		 * commit transaction
+		 * return Student data
+		 * 
+		 */
 		return null;
 	}
 
 	public void registerStudentToCourse(String email, int i) {
 		// TODO Auto-generated method stub
+		/*
+		 * create session
+		 * create transaction object
+		 * being transaction
+		 * create query
+		 * insert data into DB
+		 * commit transaction
+		 * catch errors
+		 * no returns
+		 */
 		
 	}
 
 	public List<Course> getStudentCourses(String email) {
 		// TODO Auto-generated method stub
+		/*
+		 * create new course list to pass back from method
+		 * create session
+		 * create transaction object
+		 * begin transaction
+		 * create query
+		 * fill course list with results
+		 * commit transaction
+		 * catch errors
+		 * return data
+		 */
 		return null;
 	}
 
 	@Override
 	public List<Student> getAllStudents() {
+		/*
+		 * create new student list to pass back from method
+		 * create session
+		 * create transaction object
+		 * begin transaction
+		 * create query
+		 * fill student list with results
+		 * commit transaction
+		 * catch errors
+		 * return data
+		 */
 		// TODO Auto-generated method stub
 		return null;
 	}
